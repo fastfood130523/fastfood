d3046fbde15b6bbbdc3d58332ea0475be4d202c4
unknown <service.tek.bot@mail.ru>
2023-06-08 21:25:43 +0600

    Diploy

--- fastfood/settings.py
+++ fastfood/settings.py
@@ -173,13 +173,15 @@ LOGOUT_URL = '/logout/'
 # Сброс пароля по E-Mail
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = 'MAbylmazhitov@gmail.com'
-#EMAIL_HOST_PASSWORD = 'Mm242424Gg'
+#EMAIL_HOST_USER = 'login@gmail.com'
+#EMAIL_HOST_PASSWORD = '****'
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
 #SERVER_EMAIL = EMAIL_HOST_USER
 #DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 300
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
